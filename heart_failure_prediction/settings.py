22466b61130abf24de42701af03c623ee9a7b87e
vaggos149 <vaggos149@gmail.com>
2021-12-26 17:46:42 +0200

    includes unversioned needed files

---
+++ heart_failure_prediction/settings.py
@@ -0,0 +1,4 @@
+main_project_path = 'D:/Github/Machine_Learning_Projects/heart_failure_prediction'
+
+column_rename_mapping = dict(zip(['creatinine_phosphokinase', "platelets", "ejection_fraction", "high_blood_pressure", "serum_creatinine", "serum_sodium", "DEATH_EVENT"],
+                                 ["cp", "ptls", "ef", "hbp", "sc", "ss", "death"]))
