cf3ca2c82bf17c8006d049c7d7a035df58abbe5c
v.chatzidimitriou <v.chatzidimitriou@kaizengaming.com>
2023-12-16 16:51:58 +0200

    formatting changes in data_processing.py

--- pragmatic_play/data_analysis_and_processing/data_processing.py
+++ pragmatic_play/data_analysis_and_processing/data_processing.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
 from pragmatic_play.lib.data_objects import TestingObject
 
 mode = "test"
@@ -8,15 +6,11 @@ testing_obj = TestingObject(mode=mode)
 train_data = testing_obj.read_dataframe_from_csv(csv_path_to_read="D:/Machine_Learning_Projects/titanic_survival_classification/train_test_data/train.csv")
 test_data = testing_obj.read_dataframe_from_csv(csv_path_to_read="D:/Machine_Learning_Projects/titanic_survival_classification/train_test_data/test.csv")
 
-# A. DATA EXPLORATION
-
 # Print Info
 print("Train data info: {}".format(train_data.info()))
 print("Test_data info: {}".format(test_data.info()))
 
-
 # feature engineering
-# median imputation for aGE IS FINE,
 train_data_median_age = train_data['Age'].median()
 
 train_data['Age'].fillna(train_data_median_age, inplace=True)
@@ -24,17 +18,17 @@ test_data['Age'].fillna(train_data_median_age, inplace=True)
 
 train_data_median_fare = train_data.Fare.median()
 test_data['Fare'].fillna(train_data_median_fare, inplace=True)
-# most frequent for Embarked is fine, but since there are only two missing vlaues, we will simply drop the rows.
+
 train_data.dropna(axis=0, subset=['Embarked'], inplace=True)
 test_data.dropna(axis=0, subset=['Embarked'], inplace=True)
+
+
 # drop Cabin from train and test
 train_data = train_data.drop('Cabin', axis=1)
 test_data = test_data.drop('Cabin', axis=1)
-
-# repeat results for train and test set
 
 # Save Results
 testing_obj.write_dataframe_to_csv(dataset=train_data, dataset_name="train_data_processed")
 testing_obj.write_dataframe_to_csv(dataset=test_data, dataset_name="test_data_processed")
 # proceed with modeling
-# Random Forest, Dense Network, xgb
+
