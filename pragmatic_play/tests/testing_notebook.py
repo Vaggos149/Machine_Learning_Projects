10d868f86932ca10285451f55d34580630d539f8
v.chatzidimitriou <v.chatzidimitriou@kaizengaming.com>
2023-12-16 13:34:45 +0200

    feat: includes testing_notebook.py for initial commits

---
+++ pragmatic_play/tests/testing_notebook.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import nltk
+import sklearn
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+train_data = pd.read_csv("/titanic_survival_classification/train_test_data/train.csv")
+test_data = pd.read_csv("/titanic_survival_classification/train_test_data/test.csv")
+
+
+# A. DATA EXPLORATION
+
+## Print Info
+print("Train data info: {}".format(train_data.info()))
+print("Test_data info: {}".format(test_data.info()))
+
+## Visualize columns
+
+
+## Analyze data, write comments
+
+## Replace null values
+
+## Save Results
+
