c852e753b9d9f04346b4f5eb8b72f273806bee16
v.chatzidimitriou <v.chatzidimitriou@kaizengaming.com>
2023-12-18 12:27:27 +0200

    removes unnecessary import from modeling.py

--- pragmatic_play/lib/modeling.py
+++ pragmatic_play/lib/modeling.py
@@ -3,7 +3,7 @@ from typing import List, Union
 
 import pandas as pd
 
-from sklearn.model_selection import cross_validate, GridSearchCV, train_test_split
+from sklearn.model_selection import cross_validate, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 import xgboost as xgb
 
