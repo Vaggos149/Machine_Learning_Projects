8e156575bd20bbad00ca8e54eab5b4bc971ff8c4
v.chatzidimitriou <v.chatzidimitriou@kaizengaming.com>
2023-12-18 13:51:34 +0200

    includes init file in pragmatic play folder

---
+++ pragmatic_play/interview/main_interview_file.py
@@ -0,0 +1,9 @@
+import sklearn
+import tensorflow as tf
+import keras
+import numpy as np
+import pandas as pd
+
+train_data = pd.read_csv('D:/Machine_Learning_Projects/pragmatic_play/data_storage/train_data_processed.csv')
+
+
