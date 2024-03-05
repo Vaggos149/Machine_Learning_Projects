b63e71193661b8b0715e5119a9a9e19b2612769e
v.chatzidimitriou <v.chatzidimitriou@kaizengaming.com>
2024-03-05 14:51:21 +0200

    includes modifications in sklearn testing file

--- testing_directory/testing_file.py
+++ testing_directory/testing_file.py
@@ -1,15 +1,32 @@
-import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+
+documents = ["Dog bites man.", "Man bites dog.", "Dog eats meat.", "Man eats food."]  #Same as the earlier notebook
+processed_docs = [doc.lower().replace(".", "") for doc in documents]
+processed_docs
+
+count_vect = CountVectorizer()
+
+# Build a BOW representation for the corpus
+bow_rep = count_vect.fit_transform(processed_docs)
+# Look at the vocabulary mapping
+print("Our vocabulary: ", count_vect.vocabulary_)
 
 
-x = [1, 2, 3, 4, 5]
-array_of_x = np.array(x)
+print("BoW representation for 'dog bites man': ", bow_rep[0].toarray())
+print("BoW representation for 'man bites dog: ",bow_rep[1].toarray())
+#Get the representation using this vocabulary, for a new text
+temp = count_vect.transform(["dog and dog are friends"])
+print("Bow representation for 'dog and dog are friends':",
+temp.toarray())
 
-print(array_of_x)
-print("This is to have a new point of view in the commits")
 
-print("for a new purpose, I am printing out this branch")
-print("This is a new commit")
+#n-gram vectorization example with count vectorizer and uni, bi, trigrams
+count_vect = CountVectorizer(ngram_range=(1,3))
+#Build a BOW representation for the corpus
+bow_rep = count_vect.fit_transform(processed_docs)
 
-print("commiting new in pushing print line")
-print("commiting new in pushing print line 2")
-
+#Look at the vocabulary mapping
+print("Our vocabulary: ", count_vect.vocabulary_)
+#Get the representation using this vocabulary, for a new text
+temp = count_vect.transform(["dog and dog are friends"])
+print("Bow representation for 'dog and dog are friends':", temp.toarray())
